df77f5c824a95f3e34d3e61e6172f589a0336371
a-out <richbone227@gmail.com>
2014-11-17 18:45:06 +0000

    scrapped keypress directive

--- js/controllers.js
+++ js/controllers.js
@@ -3,19 +3,19 @@
       'timer'      
    ]);
 
+   var keypress = function(event) {
+      console.log(event.which);
+   };
+
    var splitTimerController = function($scope) {
 
       $scope.splits = [
          { name: "Majula Bonfire", time: "1:00" }
       ];
-
-      console.log("hello world");
-
-      $scope.pressed = function() {
-         console.log("space pressed");
-      };
+     
    };
 
    app.controller("SplitTimerController",
-      ["$scope", splitTimerController]);
+      ["$scope", splitTimerController]);  
+
 })();
