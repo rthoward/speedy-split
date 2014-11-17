2e774e1933acb1740ba3af11f43148ec95a33281
a-out <richbone227@gmail.com>
2014-11-17 18:56:03 +0000

    space pressed event using $broadcast

--- js/controllers.js
+++ js/controllers.js
@@ -12,10 +12,22 @@
       $scope.splits = [
          { name: "Majula Bonfire", time: "1:00" }
       ];
-     
+
+      $scope.$on('space', function() {
+         console.log("space hit");
+      });
+   };
+
+   var spacePressedController = function($scope, $document) {
+      $document.bind('keypress', function(event) {
+         if (event.which === 32) {
+            $scope.$broadcast('space');
+         }
+      });
    };
 
    app.controller("SplitTimerController",
-      ["$scope", splitTimerController]);  
-
+      ["$scope", splitTimerController]);
+   app.controller("SpacePressedController",
+      ["$scope", "$document", spacePressedController]);
 })();
